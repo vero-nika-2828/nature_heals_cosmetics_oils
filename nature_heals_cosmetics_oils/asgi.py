5c224a12abf07ebfb8ce7c97101560759d07ad1c
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-09-04 15:11:45 +0000

    Initial commit

---
+++ nature_heals_cosmetics_oils/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for nature_heals_cosmetics_oils project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nature_heals_cosmetics_oils.settings')
+
+application = get_asgi_application()
